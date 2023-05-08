6724df40db9f4a8c4aabe3d20a694ed7a30d975e
Pheon-Dev <devpheon@gmail.com>
2023-05-08 13:22:04 +0300

    chore: componentising routes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::extract::Query;
+use axum::extract::{Path, Query};
 use serde::Deserialize;
 use std::net::SocketAddr;
 
@@ -8,15 +8,23 @@ use axum::Router;
 
 #[tokio::main]
 async fn main() {
-    let routes_manta = Router::new().route("/api", get(handler_manta));
+    let api_route = Router::new().merge(api_routes());
+
     // region: Start Server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
     axum::Server::bind(&addr)
-        .serve(routes_manta.into_make_service())
+        .serve(api_route.into_make_service())
         .await
         .unwrap();
     // endregion: Start Server
+}
+
+// region: Routes
+fn api_routes() -> Router {
+    Router::new()
+        .route("/api1", get(handler_endpoint))
+        .route("/api2/:endpoint", get(handler_endpoints))
 }
 
 #[derive(Debug, Deserialize)]
@@ -25,10 +33,18 @@ struct MantaParams {
 }
 
 // region: Handler
-async fn handler_manta(Query(params): Query<MantaParams>) -> impl IntoResponse {
-    println!("->> {:<12} - handler_manta - {params:?}", "HANDLER");
+
+// .e.g '/api/c2b'
+async fn handler_endpoints(Path(endpoint): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_endpoints - {endpoint:?}", "HANDLER");
+    Html(format!("<h1>APIv2: {endpoint}</h1>"))
+}
+
+// .e.g '/api?endpoint=c2b'
+async fn handler_endpoint(Query(params): Query<MantaParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_endpoint - {params:?}", "HANDLER");
 
     let endpoint = params.endpoint.as_deref().unwrap_or("c2b");
-    Html(format!("<h1>API: {endpoint}!</h1>"))
+    Html(format!("<h1>APIv1: {endpoint}</h1>"))
 }
 // endregion: Handler
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -6,8 +6,9 @@ use serde_json::json;
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://127.0.0.1:8080")?;
-    hc.do_get("/api").await?.print().await?;
-    hc.do_get("/api?endpoint=c2b").await?.print().await?;
+    hc.do_get("/api1").await?.print().await?;
+    hc.do_get("/api1?endpoint=c2b").await?.print().await?;
+    hc.do_get("/api2/b2c").await?.print().await?;
     // hc.do_get("/src/main.rs").await?.print().await?;
     // let req_login = hc.do_post(".api/login", json!({"username": "demo1", "pwd": "welcome"}));
     Ok(())
