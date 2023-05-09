c697e1b7be911fcacbfaec47d2932541fd7f8322
Pheon-Dev <devpheon@gmail.com>
2023-05-09 10:37:23 +0300

    feat: REST handlers for the c2b simulate model

---
+++ src/web/c2b_sim_routes.rs
@@ -0,0 +1,36 @@
+use axum::extract::{Path, State};
+use axum::Json;
+
+use crate::model::{C2BSimulateRequest, C2BSimulateRequestCreate, ModelController};
+use crate::Result;
+
+// region: --- REST Handler
+async fn create_c2b_sim_req(
+    State(mc): State<ModelController>,
+    Json(c2b_src): Json<C2BSimulateRequestCreate>,
+) -> Result<Json<C2BSimulateRequest>> {
+    println!("->> {:<12} - create_c2b_sim_req", "HANDLER");
+    let c2b_sim_req = mc.c2b_simulate_create(c2b_src).await?;
+    Ok(Json(c2b_sim_req))
+}
+
+async fn list_c2b_sim_req(
+    State(mc): State<ModelController>,
+) -> Result<Json<Vec<C2BSimulateRequest>>> {
+    println!("->> {:<12} - list_c2b_sim_reqs", "HANDLER");
+    let c2b_sim_reqs = mc.c2b_simulate_list().await?;
+    Ok(Json(c2b_sim_reqs))
+}
+
+async fn delete_c2b_sim_req(
+    State(mc): State<ModelController>,
+    // ctx: Ctx,
+    Path(id): Path<u64>,
+) -> Result<Json<C2BSimulateRequest>> {
+    println!("->> {:<12} - delete_c2b_sim_req", "HANDLER");
+    // let c2b_sim_req = mc.c2b_simulate_delete(ctx, id).await?;
+    let c2b_sim_req = mc.c2b_simulate_delete(id).await?;
+    Ok(Json(c2b_sim_req))
+}
+
+// endregion: --- REST Handler
--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,2 +1,3 @@
+pub mod c2b_sim_routes;
 pub mod login_routes;
 pub const AUTH_TOKEN: &str = "auth-token";
