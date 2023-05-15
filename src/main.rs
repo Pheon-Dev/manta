415e59b9c3af1a30883074f86da5982c5af6cc83
Pheon-Dev <devpheon@gmail.com>
2023-05-15 14:00:11 +0300

    [~] response mapper for eventual errors

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@
 use crate::model::ModelController;
 use axum::extract::{Path, Query};
 use serde::Deserialize;
+use serde_json::json;
 use std::net::SocketAddr;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 pub use self::error::{Error, Result};
 
 use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
-use axum::{middleware, Router};
+use axum::{middleware, Json, Router};
 
 mod ctx;
 mod error;
@@ -30,6 +32,10 @@ async fn main() -> Result<()> {
         .merge(web::login_routes::routes())
         .nest("/api", routes_apis)
         .layer(middleware::map_response(main_response_mapper))
+        .layer(middleware::from_fn_with_state(
+            mc.clone(),
+            web::mw_auth::mw_ctx_resolver,
+        ))
         .layer(CookieManagerLayer::new())
         .fallback_service(static_routes());
 
@@ -47,8 +53,34 @@ async fn main() -> Result<()> {
 
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+    let uuid = Uuid::new_v4();
+
+    // -- Get the eventual response error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    // -- build a new response if there is a client error
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                    "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid":uuid.to_string(),
+                }
+            });
+
+            println!("  ->> client_error_body: {client_error_body}");
+
+            // build the new response from the client_error_body
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    // -- TODO: build and log the server log line
+    println!("  ->> server log line - {uuid} - Error: {service_error:?}");
+
     println!();
-    res
+    error_response.unwrap_or(res)
 }
 
 // region: Routes
